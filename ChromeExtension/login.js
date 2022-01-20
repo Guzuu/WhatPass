5d5e0f4e1a69f243051986a5d29e204332196c8a
Mateusz Guz <mateuszguz54@gmail.com>
2022-01-20 02:29:10 +0100

    Base API, Extension popup with login, logout, saving login state, retrieving and saving token

---
+++ ChromeExtension/login.js
@@ -0,0 +1,50 @@
+document.getElementById("submitLogin").addEventListener("click", login);
+document.getElementById("logOut").addEventListener("click", logout);
+
+var tokenKey = "tokenInfo";
+
+chrome.storage.local.get("userName", function(data) {
+    if(data.userName != null) {
+        $('.userName').text(data.userName);
+        $('.userInfo').css('display', 'block');
+        $('.loginForm').css('display', 'none');
+    }
+});
+
+function login(e) {
+    e.preventDefault();
+    var loginData = {
+        grant_type: 'password',
+        username: $('#email').val(),
+        password: $('#password').val()
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: 'https://localhost:44366/token',
+        data: loginData,
+        success: function (data) {
+            $('.userName').text(data.userName);
+            $('.userInfo').css('display', 'block');
+            $('.loginForm').css('display', 'none');
+            
+            chrome.storage.local.set({tokenKey: data.access_token, "userName": data.userName}, function() {
+                console.log(tokenKey + ' value is set to ' + data.access_token);
+            });
+        },
+        fail: function (data) {
+            alert('Error on login');
+        }
+    });
+}
+
+function logout(e) {
+    e.preventDefault();
+    chrome.storage.local.clear(function() {
+        $('.userName').text("");
+        $('.userInfo').css('display', 'none');
+        $('.loginForm').css('display', 'block');
+
+        console.log('Removed ' + tokenKey);
+    });
+}
---
+++ WebAPI/WhatPass/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WhatPass.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
