8f629d0aebd482274c41db90a4d8020ce1a0cb9b
Mateusz Guz <mateuszguz54@gmail.com>
2022-01-21 15:25:32 +0100

    getCreds event now executes correctly after submiting form

--- ChromeExtension/getcreds.js
+++ ChromeExtension/getcreds.js
@@ -1,10 +1,10 @@
 document.getElementById("getCreds").addEventListener("click", getCreds);
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender) {
+    function (request, sender) {
         console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
+            "from a content script:" + sender.tab.url :
+            "from the extension");
         console.log(request.loginData);
     }
 );
@@ -20,23 +20,25 @@ function getCreds() {
         });
     });
 }
+
 const findCreds = () => {
     var forms = document.forms;
     var loginData = {
         username: "",
         password: ""
     }
+
     Array.prototype.forEach.call(forms, form => {
-        Array.prototype.forEach.call(form.elements, element =>{
-            if(element.type == "text" && loginData.username == "" && loginData.password == ""){
-                loginData.username = element.value;
-            }
-            if(element.type == "password" && loginData.password == ""){
-                loginData.password = element.value;
-                form.addEventListener("submit", function (){
-                    chrome.runtime.sendMessage({loginData}); 
-                });
-            }
+        form.addEventListener("submit", function () {
+            Array.prototype.forEach.call(form.elements, element => {
+                if (element.type == "text" && loginData.username == "" && loginData.password == "") {
+                    loginData.username = element.value;
+                }
+                if (element.type == "password" && loginData.password == "") {
+                    loginData.password = element.value;
+                }
+            });
+            chrome.runtime.sendMessage({ loginData });
         });
     });
 }
