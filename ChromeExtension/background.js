7c23d6f1f8d441a5beb0637fc729352d468b83d6
Mateusz Guz <mateuszguz54@gmail.com>
2022-01-23 17:21:01 +0100

    Moved getCreds execution to background service worker. Added notification button event listeners.

--- ChromeExtension/background.js
+++ ChromeExtension/background.js
@@ -1,10 +1,11 @@
+//if user logged in run script.
 chrome.runtime.onMessage.addListener(
     function (request, sender) {
         console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
             "from the extension");
         console.log(request.loginData);
-        chrome.notifications.clear(sender.tab.url);
+        chrome.notifications.clear(sender.tab.url); //temporary for testing
         chrome.notifications.create(sender.tab.url, {
             type: 'basic',
             iconUrl: 'WhatPass.png',
@@ -21,5 +22,45 @@ chrome.runtime.onMessage.addListener(
                 }
             ]
         });
+        chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
+            if (buttonIndex == 0) {
+                console.log('save password');
+            }
+        });
+        // chrome.notifications.onClosed.addListener(function (notificationId, byUser){  Doesnt work, not important rn
+        //     chrome.notifications.clear(notificationId);
+        //     console.log('notification closed');
+        // });
     }
 );
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.status != "loading") { //temporary
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            func: injectFindAndSendEvent
+        });
+    }
+});
+
+const injectFindAndSendEvent = () => {
+    var forms = document.forms;
+    var loginData = {
+        username: "",
+        password: ""
+    }
+
+    Array.prototype.forEach.call(forms, form => {
+        form.addEventListener("submit", function () {
+            Array.prototype.forEach.call(form.elements, element => {
+                if (element.type == "text" && loginData.username == "" && loginData.password == "") {
+                    loginData.username = element.value;
+                }
+                if (element.type == "password" && loginData.password == "") {
+                    loginData.password = element.value;
+                }
+            });
+            chrome.runtime.sendMessage({ loginData });
+        });
+    });
+}
