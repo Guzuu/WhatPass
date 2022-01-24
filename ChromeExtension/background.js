adc125380f9e1d7270b987818f38e510fc910394
Mateusz Guz <mateuszguz54@gmail.com>
2022-01-24 02:44:29 +0100

    credentials migration, post request to save credentials

--- ChromeExtension/background.js
+++ ChromeExtension/background.js
@@ -1,10 +1,16 @@
-//if user logged in run script.
 chrome.runtime.onMessage.addListener(
     function (request, sender) {
         console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
             "from the extension");
         console.log(request.loginData);
+
+        var passData = {
+            url: sender.tab.url,
+            username: request.loginData.username,
+            password: request.loginData.password
+        };
+
         chrome.notifications.clear(sender.tab.url); //temporary for testing
         chrome.notifications.create(sender.tab.url, {
             type: 'basic',
@@ -24,7 +30,20 @@ chrome.runtime.onMessage.addListener(
         });
         chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
             if (buttonIndex == 0) {
-                console.log('save password');
+                chrome.storage.local.get("tokenInfo", function (data) {
+                    const params={
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + data.tokenInfo
+                        },
+                        body: JSON.stringify(passData),
+                        method: "POST"
+                    }
+                    fetch("https://localhost:44366/api/credentials", params)
+                    .then(data => {return data.json()})
+                    .then(res=>{console.log(res)})
+                    .catch(error=>console.log(error));
+                });
             }
         });
         // chrome.notifications.onClosed.addListener(function (notificationId, byUser){  Doesnt work, not important rn
@@ -35,12 +54,16 @@ chrome.runtime.onMessage.addListener(
 );
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (tab.status != "loading") { //temporary
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            func: injectFindAndSendEvent
-        });
-    }
+    chrome.storage.local.get("userName", function (data) {
+        if (data.userName != null) {
+            if (tab.status != "loading") { //temporary
+                chrome.scripting.executeScript({
+                    target: { tabId: tabId },
+                    func: injectFindAndSendEvent
+                });
+            }
+        }
+    });
 });
 
 const injectFindAndSendEvent = () => {
---
+++ WebAPI/WhatPass/Migrations/202201240120591_credentials.cs
@@ -0,0 +1,28 @@
+﻿namespace WhatPass.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class credentials : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CredentialsModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Url = c.String(),
+                        Username = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.CredentialsModels");
+        }
+    }
+}
